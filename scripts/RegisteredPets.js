eaa5a869604e19827fc8257cdb9a071b051fd957
Ashton Monzi <anmonzi@gmail.com>
2021-04-26 13:28:54 -0500

    added event listener for pets, when pet name is clicked it'll show pet name and walker name

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,37 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        if (clickEvent.target.id.startsWith("pet")) {
+            const [, petId] = clickEvent.target.id.split("--")
+            const petIdNumber = parseInt(petId)
+
+            for (const pet of pets) {
+                if (pet.id === petIdNumber) {
+                    const petWalkerId = pet.walkerId
+
+                    for (const walker of walkers) {
+                        if (walker.id === petWalkerId) {
+                            window.alert(`${pet.name} is being walked by ${walker.name}`)
+                        }
+                    }
+                }
+            } 
+        }
+    }
+)
+
+
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
