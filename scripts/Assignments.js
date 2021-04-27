b874fd7b6a0522c07a0d5316e17309a4d4232b2b
Ashton Monzi <anmonzi@gmail.com>
2021-04-27 13:09:11 -0500

    refactored assignment output to target new city object

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,8 +1,9 @@
-import { getPets, getWalkers } from "./database.js"
+import { getCities, getPets, getWalkers } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
+const cities = getCities()
 
 
 // Function whose responsibility is to find the walker assigned to a pet
@@ -18,16 +19,32 @@ const findWalker = (pet, allWalker) => {
     return petWalker
 }
 
+// Function whose responsibility is to find the city name assigned to a walker
+const findCity = (walker, allCities) => {
+    let foundCity = null
+
+    for (const city of allCities) {
+        if (city.id === walker.cityId) {
+            foundCity = city
+        }
+    }
+
+    return foundCity
+}
+
+
+
 export const Assignments = () => {
     let assignmentHTML = ""
     assignmentHTML = "<ul>"
 
     for (const currentPet of pets) {
         const currentPetWalker = findWalker(currentPet, walkers)
+        const currentCity = findCity(currentPetWalker, cities)
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${currentCity.name}
             </li>
         `
     }
